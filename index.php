96f73e6e4c5fd020c5eb8b6ab7eac433d32a4487
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-01-30 15:55:45 -0600

    session two lab

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<style>
+h1 {text-align: center;}
+
+</style>
+        <title>Jennifer Thomas - STIM 3320 - Session 2 LAB</title>
+
+   <body>
+  		<h1><b>Jennifer Thomas</b></h1> 		
+		  <hr/>
+	  
+	<h2>About Me</h2>
+	  <p><b><i>Hi y&#39;all, my name is Jennifer Thomas, and I am studying Web Page Design at Dallas Baptist University, primarily to satisfy requirements for my MIS BS degree, and secondarily because I enjoy technology, learning, and deepening skills in all areas. </i></b></p>
+	  
+	  <p style="color:red ; font-size:30px; font-family:verdana"</p>So far I am learning a lot!</p>
+	  
+	<h2>My Favorite Quote</h2>
+	  <blockquote>&quot;Tell me and I forget. Teach me and I remember. Involve me and I learn.&quot; –Benjamin Franklin </blockquote>
+	  
+	<h2> Things I enjoy</h2>
+	<ol>
+	  <li>technology</li>
+	  <li>working out</li>
+	  <li>spending time with my dogs</li>
+	  <li>learning</li>
+	</ol>
+	  
+	<h2>Adjectives that describe me</h2>
+	<ul>
+	<li>Inquisitive</li>
+	<li>Motivated</li>
+	<li>Dependable</li>
+	<li>Empathetic</li>
+	<li>Open-minded</li>
+	</ul>
+	  
+	<h3> My Favorite Web Sites</h3>
+	<a href="https://test-php-dbu.herokuapp.com/"><mark>A copy of this page running on the internet</mark></a><br>
+	<a href="https://www.google.com">Google</a><br>	
+	<a href="https://threatpost.com">ThreatPost</a><br>
+	<a href="https://dbu.onelogin.com/portal/">My DBU<a/><br>
+	<a href="https://github.com/">My GitHub Repository</a><br>
+	
+	<p></p>
+	  
+	  
+   </body>
+   
+	<footer>
+	<p><small><i><b>Author:</b>Jennifer Thomas<br>
+	<b>Class:</b>STIM 3320<br>
+	<b>Date:</b>January 30th, 2021</i></small><p>
+	</footer>
+</html>
